fb223830960a8c5510d4cb403b8887b952b53b6f
Pagan Gazzard <page@balena.io>
2022-10-12 15:46:11 +0100

    Update dependencies/add linting/typechecking
    Change-type: patch

--- bin/cli.js
+++ bin/cli.js
@@ -1,50 +1,56 @@
 #!/usr/bin/env node
 
-const capitano = require('capitano')
-const balenaVersionist = require('../lib/index.js')
+const capitano = require('capitano');
+const balenaVersionist = require('../lib/index.js');
 
 capitano.command({
-  signature: '[path]',
-  description: 'Run versionist in path',
-  options: [{
-    signature: 'silent',
-    boolean: true,
-    description: 'No output, defaults to false'
-  }, {
-    signature: 'title',
-    parameter: 'title',
-    alias: ['t'],
-    description: 'set changelog title'
-  }],
-  action: (params, options) => {
-    const path = params.path || '.'
-    balenaVersionist.runBalenaVersionist(path, options)
-  }
-})
+	signature: '[path]',
+	description: 'Run versionist in path',
+	options: [
+		{
+			signature: 'silent',
+			boolean: true,
+			description: 'No output, defaults to false',
+		},
+		{
+			signature: 'title',
+			parameter: 'title',
+			alias: ['t'],
+			description: 'set changelog title',
+		},
+	],
+	action: (params, options) => {
+		const path = params.path || '.';
+		balenaVersionist.runBalenaVersionist(path, options);
+	},
+});
 
 capitano.command({
-  signature: 'set <version> [path]',
-  description: 'Run versionist in path with set versions',
-  options: [{
-    signature: 'silent',
-    boolean: true,
-    description: 'No output, defaults to false'
-  },{
-    signature: 'title',
-    parameter: 'title',
-    alias: ['t'],
-    description: 'set changelog title'
-  }],
-  action: (params, options) => {
-    const path = params.path || '.'
-    balenaVersionist.runBalenaVersionist(path, {
-      version: params.version
-    })
-  }
-})
+	signature: 'set <version> [path]',
+	description: 'Run versionist in path with set versions',
+	options: [
+		{
+			signature: 'silent',
+			boolean: true,
+			description: 'No output, defaults to false',
+		},
+		{
+			signature: 'title',
+			parameter: 'title',
+			alias: ['t'],
+			description: 'set changelog title',
+		},
+	],
+	action: (params, options) => {
+		const path = params.path || '.';
+		balenaVersionist.runBalenaVersionist(path, {
+			version: params.version,
+		});
+	},
+});
 
-capitano.run(process.argv, err => {
-  if (err != null) {
-    throw err
-  }
-})
+capitano.run(process.argv, (err) => {
+	if (err != null) {
+		throw err;
+	}
+});
